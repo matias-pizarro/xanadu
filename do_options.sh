0000704937f7aacfd1d3cc3597f79e7d76c37e00
matias pizarro <matias@pizarro.net>
2016-01-16 22:59:51 +0100

    Rename DO creation playbook

--- do_options.sh
+++ do_options.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-endpoints=(sizes regions images ssh_keys)
+endpoints=(sizes regions images ssh_keys floating_ips account)
 for i in "${endpoints[@]}"; do
   url=`printf "https://api.digitalocean.com/v2/%s" $i`
   curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_API_TOKEN}" $url | python -mjson.tool
