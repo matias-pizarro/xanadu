a97d5e06098a4536b86e71fbe33abdfe844398a0
matias pizarro <matias@pizarro.net>
2016-03-03 23:45:44 +0100

    Go about allowing programmatical parametrization of jails in a slightly more orderly way

--- hosts/jails.py
+++ hosts/jails.py
@@ -13,25 +13,41 @@ from ansible.vars.hostvars import HostVars
 
 PLAYBOOK_PATH = './playbooks/site.yml'
 STATIC_HOSTS_PATH = './hosts/static_hosts'
-RUN = 1
 
-if len(sys.argv) > 1 and sys.argv[1] == 'flatlist':
-    RUN = 2
 
-loader = DataLoader()
-variable_manager = VariableManager()
-variable_manager.get_vars(loader=loader)
+def state_of_the_union():
+    """On the first run HOSTS_LIST resolves to the path containing all static
+    hosts definitions. On the second run it contains the list of static hosts and their jails"""
 
-inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list=STATIC_HOSTS_PATH)
-variable_manager.set_inventory(inventory)
+    if len(sys.argv) > 1 and sys.argv[1] == 'flatlist':
+        RUN = 2
+        HOSTS_LIST = json.loads(sys.argv[2])
+    else:
+        RUN = 1
+        HOSTS_LIST = STATIC_HOSTS_PATH
+    return RUN, HOSTS_LIST
 
-if not os.path.exists(PLAYBOOK_PATH):
-    print '[INFO] The playbook does not exist'
-    sys.exit()
 
-inventory.set_playbook_basedir(os.path.dirname(PLAYBOOK_PATH))
+def first_run(inventory):
+        """On first running this script extracts the list of jails from each host and calls itself
+        again so that on the next run we can access both host and jail vars. If no jails are defined
+        it exits."""
+        hosts = []
+        for host in inventory.get_hosts():
+            hosts.append(host.name)
+            host_vars = inventory.get_host_vars(host)
+            jails_list = host_vars.get('jails', [])
+            for jail_name in jails_list:
+                hosts.append(jail_name)
+        if len(hosts) > len(inventory.get_hosts()):
+            return subprocess.check_output(["hosts/jails.py", "flatlist", json.dumps(hosts)])
+        else:
+            return ''
 
-if RUN == 1:
+
+def second_run(inventory):
+    """On its second run this script has access to both host and jail vars. Any suitable logic
+    to programmatically parametrize jails can be inserted here."""
     jails = {
         "jails": {
             "hosts": []
@@ -43,19 +59,37 @@ if RUN == 1:
 
     for host in inventory.get_hosts():
         host_vars = inventory.get_host_vars(host)
-        jails_list = host_vars.get('jails', [])
-        if not isinstance(jails_list, list):
-            jails_list = list(jails_list)
-        for jail in jails_list:
-            jails['jails']['hosts'].append(jail)
-            jails['_meta']['hostvars'][jail] = {'jail_host': host.name, 'hosting': host_vars.get('hosting', '')}
+        if host_vars['host_type'] == 'jail_host':
+            jails_list = host_vars.get('jails', [])
+            for jail_name in jails_list:
+                jail = inventory.get_host(jail_name)
+                jail_vars = inventory.get_host_vars(jail)
+                jails['jails']['hosts'].append(jail.name)
+                hosting = host_vars.get('hosting', '')
+                jails['_meta']['hostvars'][jail.name] = {'jail_host': host.name,
+                                                         'hosting': hosting}
+    return json.dumps(jails)
 
-    if len(jails['jails']['hosts']):
-        jails = subprocess.check_output(["hosts/jails.py", "flatlist", json.dumps(jails)])
-        print(jails)
-else:
-    jails = json.loads(sys.argv[2])
-    for host, config in jails['_meta']['hostvars'].iteritems():
-        # Do something
-        pass
-    print(json.dumps(jails))
+
+def main():
+    RUN, HOSTS_LIST = state_of_the_union()
+
+    loader = DataLoader()
+    variable_manager = VariableManager()
+    variable_manager.get_vars(loader=loader)
+
+    inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list=HOSTS_LIST)
+    variable_manager.set_inventory(inventory)
+
+    if not os.path.exists(PLAYBOOK_PATH):
+        print '[INFO] The playbook does not exist'
+        sys.exit()
+
+    inventory.set_playbook_basedir(os.path.dirname(PLAYBOOK_PATH))
+
+    output = first_run(inventory) if RUN == 1 else second_run(inventory)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
