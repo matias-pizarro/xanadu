f0803f4abc9038c4e64a70a036d3901eef3105ac
matias pizarro <matias@pizarro.net>
2016-03-08 23:53:04 +0100

    Simplify dynamic inventory

--- hosts/site.py
+++ hosts/site.py
@@ -6,90 +6,93 @@ import json
 import os
 import subprocess
 import sys
+import tempfile
 
-from ansible.parsing.dataloader import DataLoader
-from ansible.vars import VariableManager
 from ansible.inventory import Inventory
+from ansible.parsing.dataloader import DataLoader
+from ansible.utils.vars import combine_vars
+from ansible.vars import VariableManager
 from ansible.vars.hostvars import HostVars
 
 PLAYBOOK_PATH = './playbooks/site.yml'
 STATIC_HOSTS_PATH = './static_hosts'
 
 
-def tree():
-    return collections.defaultdict(tree)
-
-def group():
-    return collections.defaultdict(dict, {"hosts": []})
-
-def state_of_the_union():
-    """On the first run HOSTS_LIST resolves to the path containing all static
-    hosts definitions. On the second run it contains the list of static hosts and their jails"""
-
-    if len(sys.argv) > 1 and sys.argv[1] == 'flatlist':
-        RUN = 2
-        HOSTS_LIST = json.loads(sys.argv[2])
+def get_hosts_list():
+    if len(sys.argv) > 2 and sys.argv[1] == '--list':
+        return sys.argv[2]
     else:
-        RUN = 1
-        HOSTS_LIST = STATIC_HOSTS_PATH
-    return RUN, HOSTS_LIST
+        return STATIC_HOSTS_PATH
 
 
-def first_run(inventory):
-        """On first running this script extracts the list of jails from each host and calls itself
-        again so that on the next run we can access both host and jail vars. If no jails are defined
-        it exits."""
-        hosts = []
+def update(inventory):
+    """First computes a list of hosts and their jails, assigning them to relevant group,
+    then refreshes the inventory"""
+    handle, path = tempfile.mkstemp(dir='./', text=True)
+    with open(path, 'w') as host_list:
+        jail_hosts = set()
+        hosts = set()
+        jails = set()
         for host in inventory.get_hosts():
-            hosts.append(host.name)
-            jails_list = host.vars.get('jails', [])
-            for jail_name in jails_list:
-                hosts.append(jail_name)
-        if len(hosts) > len(inventory.get_hosts()):
-            return subprocess.check_output(["hosts/site.py", "flatlist", json.dumps(hosts)])
-        else:
-            return "{}"
+            jail_hosts.update([host.name])
+            hosts.update([host.name])
+            jails.update(host.vars.get('jails', []))
+        hosts.update(jails)
+        for host in hosts:
+            host_list.write(host + '\n')
+        host_list.write('[jail_host]\n')
+        for host in jail_hosts:
+            host_list.write(host + '\n')
+        host_list.write('[jail]\n')
+        for host in jails:
+            host_list.write(host + '\n')
+        host_list.write('[freebsd:children]\njail\njail_host')
+    inventory.host_list = host_list.name
+    inventory.refresh_inventory()
+    os.remove(path)
 
 
-def second_run(inventory):
-    """On its second run this script has access to both host and jail vars. Any suitable logic
-    to programmatically parametrize jails can be inserted here."""
-    output = {
-        "jail_hosts": group(),
-        "jails": group(),
-        "_meta": {"hostvars": tree()}
-    }
+def set_vars(inventory):
+    """Sets relevant variables"""
 
-    for host in inventory.get_hosts():
+    jail_hosts = inventory.get_group('jail_host')
+    for host in jail_hosts.hosts:
         jails_list = host.vars.get('jails', [])
-        if len(jails_list):
-            output['jail_hosts']['hosts'].append(host.name)
-            host.vars['host_type'] = 'first_class'
-            host.vars['loopback_if'] = host.vars.get('loopback_if', 'lo0')
-            host.vars['jails_if'] = host.vars.get('jails_if', 'lo1')
-            host.vars['jails_if_ipv4'] = host.vars.get('jails_if_ip', '10.0.x.y')
-            host.vars['jails_if_ipv6'] = ':'.join(host.vars.get('ipv6')['address'].split(':')[0:-2] +['x', 'y'])
-            for jail_name in jails_list:
-                jail = inventory.get_host(jail_name)
-                output['jails']['hosts'].append(jail.name)
-                jail.vars['host_type'] = 'jail'
-                jail.vars['jail_host'] = host.name
-                jail.vars['hosting'] = host.vars.get('hosting', '')
-                output['_meta']['hostvars'][jail.name] = jail.vars
+        host.vars['host_type'] = 'first_class'
+        host.vars['jails_if_ipv6'] = ':'.join(host.vars.get('ipv6')['address'].split(':')[0:-2] +['x', 'y'])
+        for jail_name in jails_list:
+            jail = inventory.get_host(jail_name)
+            jail.vars['host_type'] = 'jail'
+            jail.vars['jail_host'] = host.name
+            jail.vars['hosting'] = host.vars.get('hosting', '')
         for feature in host.vars.get('features', []):
             host.vars['has_' + feature] = True
-        output['_meta']['hostvars'][host.name] = host.vars
-    return json.dumps(output)
+
+
+def output_dict(inventory):
+    output = {"_meta": {"hostvars": {}}}
+    for host in inventory.get_hosts():
+        output['_meta']['hostvars'][host.name] = host.get_vars()
+    for group in inventory.groups.values():
+        if group.name not in ('all', 'ungrouped'):
+            output[group.name] = {"hosts": [host.name for host in group.get_hosts()]}
+    return output
+
+
+def show_vars(inventory):
+    for host in inventory.get_hosts():
+        hostvars = combine_vars(host.get_group_vars(), host.vars)
+        print(json.dumps(hostvars, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
 def main():
-    RUN, HOSTS_LIST = state_of_the_union()
+    hosts_list = get_hosts_list()
 
     loader = DataLoader()
     variable_manager = VariableManager()
     variable_manager.get_vars(loader=loader)
 
-    inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list=HOSTS_LIST)
+    inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list=hosts_list)
     variable_manager.set_inventory(inventory)
 
     if not os.path.exists(PLAYBOOK_PATH):
@@ -97,8 +100,11 @@ def main():
         sys.exit()
 
     inventory.set_playbook_basedir(os.path.dirname(PLAYBOOK_PATH))
+    update(inventory)
+    set_vars(inventory)
+    # show_vars(inventory)
 
-    output = first_run(inventory) if RUN == 1 else second_run(inventory)
+    output = json.dumps(output_dict(inventory))
     print(output)
 
 
